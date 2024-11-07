11672daeb1b3955a7e61f7b675f85c0d6832612b
Oleksandr Tsurkan <aleks6699@gmail.com>
2024-11-07 03:57:21 +0200

    doc: implements  OpenAPI spec

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const yamlDocument = yaml.load(fs.readFileSync('./doc/api.yaml', 'utf8')) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, yamlDocument);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
